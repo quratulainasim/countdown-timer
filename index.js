c407fdf04ee353cf1ac0d054677f2cdaa533a962
quratulainasim <quratulainasim1234@gmail.com>
2024-06-04 19:06:04 +0500

    countdown-timer

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { differenceInSeconds } from "date-fns";
+console.log(chalk.red("COUNTDOWN TIMER IS STARTING.....\n"));
+let uInput = await inquirer.prompt([
+    {
+        name: "ans",
+        type: "number",
+        message: "Enter your time in seconds :",
+    }
+]);
+let time = uInput.ans;
+function startT(val) {
+    let initTime = new Date();
+    let timeInterval = new Date().setSeconds(initTime.getSeconds() + (val + 2));
+    let intFormat = new Date(timeInterval);
+    setInterval(() => {
+        let currentTime = new Date();
+        let timeDiff = differenceInSeconds(intFormat, currentTime);
+        //    console.log(timeDiff)  
+        if (timeDiff <= 0) {
+            process.exit();
+        }
+        let minutes = Math.floor(timeDiff / 60);
+        let seconds = Math.floor(timeDiff % 60);
+        console.log(chalk.green(`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`));
+    }, 1000); // miliseconds
+}
+startT(time);
